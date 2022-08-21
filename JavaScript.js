9f710006275dfa2c6953eb8f20a7c3df5473f862
Garry3Fingers <www.vlad78@yandex.ru>
2022-08-21 17:07:06 +0300

    Create getComputerChoice function

--- JavaScript.js
+++ JavaScript.js
@@ -1,9 +1,23 @@
 function getPlayerChoice() {
   let playerInput = prompt("Type which item you'll throw", "Rock or Paper or Scissors");
   let playerChoice = playerInput.toUpperCase();
-  if (playerChoice === "ROCK" || playerChoice === "SCISSORS" || playerChoice === "PAPER") {
+  if (playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS") {
     return playerChoice;
   } else {
     return alert("You typed incorrect item");
   }
 }
+
+const randomNumber = () => Math.floor(Math.random() * 100) + 1;
+
+function getComputerChoice() {
+  let computerChoice = randomNumber();
+  if (computerChoice <= 33) {
+    return "ROCK";
+  } else if (computerChoice > 33 && computerChoice <= 66) {
+    return "PAPER";
+  } else {
+    return 'SCISSORS';
+  }
+}
+
