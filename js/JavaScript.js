eaf5835e0f2bbfd13ff172b12480b2dce70f15a0
Garry3Fingers <www.vlad78@yandex.ru>
2022-10-01 16:47:45 +0300

    Create a running score message

--- js/JavaScript.js
+++ js/JavaScript.js
@@ -1,129 +1,158 @@
-// Player Choice Block
-
-// const playerInput = (e) => console.loge.target.textContent;
-
-// buttons.forEach((button) => {
-//   button.addEventListener('click', playerInput)
-// });
-
-
-// function getPlayerChoice() {
-//   let playerInput = getPlayerInput();
-//   let playerChoice = playerInput.toUpperCase();
-//   if (playerChoice === "ROCK" || playerChoice === "PAPER" || playerChoice === "SCISSORS") {
-//     return playerChoice;
-//   } else if (playerChoice === "CANCELED") {
-//     return "Canceled";
-//   } else {
-//     return "Wrong input"
-//   }
-// }
-
 // Computer Choice Block
 
 const randomNumber = () => Math.floor(Math.random() * 100);
 
 function getComputerChoice() {
+  
   let computerChoice = randomNumber();
+  
   if (computerChoice >= 66) {
     return "Rock";
   } else if (computerChoice >= 33) {
     return "Paper";
   } else {
     return 'Scissors';
-  }
-}
+  };
 
-// Result of Round
-const buttons = document.querySelectorAll('button');
-const divRoundResult = document.querySelector('.result-of-round');
-const paraResult = document.createElement('p');
+};
+
+// Main block of the game play
+
+const buttons = document.querySelectorAll('.shape-button button');
+
+buttons.forEach((button) => button.setAttribute('disabled', ''));
 
 buttons.forEach((button) => {
   button.addEventListener('click', playRound)
 });
 
+const divRoundResult = document.querySelector('.result-of-round');
+
+const paraResult = document.createElement('p');
+
 function playRound(e) {
+  
   let playerChoice = e.target.textContent;
+  
   let computerChoice = getComputerChoice();
+  
   switch (true) {
+
     case (playerChoice === computerChoice):
       paraResult.className = 'draw';
       paraResult.textContent = 'It\'s a draw!';
       break;
+    
     case (playerChoice === 'Rock' && computerChoice === 'Paper'):
       paraResult.className = 'lost';
       paraResult.textContent = 'You Lost the round! Paper beats Rock!';
+      computerScore++;
       break;
+    
     case (playerChoice === 'Paper' && computerChoice === 'Scissors'):
       paraResult.className = 'lost';
       paraResult.textContent = 'You Lost the round! Scissors beats Paper!';
+      computerScore++;
       break;
+    
     case (playerChoice === 'Scissors' && computerChoice === 'Rock'):
       paraResult.className = 'lost';
       paraResult.textContent = 'You Lost the round! Rock beats Scissors!';
+      computerScore++;
       break;
+    
     case (playerChoice === 'Rock' && computerChoice === 'Scissors'):
       paraResult.className = 'won';
       paraResult.textContent = 'You Won the round! Rock beats Scissors!';
+      playerScore++;
       break;
+    
     case (playerChoice === 'Paper' && computerChoice === 'Rock'):
       paraResult.className = 'won';
       paraResult.textContent = 'You Won the round! Paper beats Rock!';
+      playerScore++;
       break;
+    
     case (playerChoice === 'Scissors' && computerChoice === 'Paper'):
       paraResult.className = 'won';
-      paraResult.textContent = 'You Won the round! Scissors beats Paper!'
+      paraResult.textContent = 'You Won the round! Scissors beats Paper!';
+      playerScore++;
       break;
   }
+
   divRoundResult.appendChild(paraResult);
-}
+  
+  score.textContent = `Current match score: Player - ${playerScore}, Computer - ${computerScore}`;
+  
+  declareTheWinner();
+
+};
+
+// Running score block
+
+const divRunningScore = document.querySelector('.score-dom-container');
+
+const score = document.createElement('p');
+
+const winnerOfGame = document.createElement('p');
+
+let playerScore;
+
+let computerScore;
+
+function declareTheWinner() {
+  
+  if (playerScore === 5) {
+    
+    winnerOfGame.textContent = 'You won the match. Congratulations!!!';
+    
+    buttons.forEach((button) => button.setAttribute('disabled', ''));
+    
+    beginOfGame.textContent = 'New Game';
+
+    divRunningScore.appendChild(winnerOfGame);
+  
+  } else if (computerScore === 5) {
+    
+    winnerOfGame.textContent = 'The computer aces this match. Maybe someday it\'ll pretend that you can beat it.';
+    
+    buttons.forEach((button) => button.setAttribute('disabled', ''));
+    
+    beginOfGame.textContent = 'New Game';
+
+    divRunningScore.appendChild(winnerOfGame);
+  
+  };
+
+};
 
 
-function playGame() {
-  let playerScore = 0;
-  let computerScore = 0;
-  let score;
-  for (let i = 0; i < 5; i++) {
-    score = getResult();
-    if (score === 1) {
-      ++playerScore;
-    } else if (score === 0) {
-      ++computerScore;
-    } else if (score === "draw") {
-      playerScore += 0;
-      computerScore += 0;
-    } else {
-      return "Canceled";
-    }
-    console.log(`Current match score: Player - ${playerScore}, Computer - ${computerScore}`);
-  }
-  console.log(declareTheWinner(playerScore, computerScore));
-}
+// Start of the game block
 
+const beginOfGame = document.querySelector('.start-game button');
 
-function declareTheWinner(playerScore, computerScore) {
-  if (playerScore > computerScore) {
-    return "You won the match. Congratulations!!!";
-  } else if (playerScore < computerScore) {
-    return "The computer aces this match. Maybe someday it'll pretend that you can beat it.";
-  } else {
-    return "It's a draw. Not bad for human being.";
-  }
-}
+beginOfGame.addEventListener('click', startGame);
 
+function startGame() {
+  
+  buttons.forEach((button) => button.removeAttribute('disabled', ''));
+  
+  clearScore();
+  
+  divRunningScore.appendChild(score);
+  
+  score.textContent = `Current match score: Player - ${playerScore}, Computer - ${computerScore}`;
 
-function getResult() {
-  roundResult = playRound(getPlayerChoice(), getComputerChoice());
-  console.log(roundResult);
-  score = roundResult.charAt(4);
-  if (score === "W") {
-    return 1;
-  } else if (score === "L" || score === "m") {
-    return 0;
-  } else if (score === " ") {
-    return "draw"
-  } else {
-    return "Canceled";
-  }
-}
+  beginOfGame.textContent = 'Restart Game';
+
+ // divRoundResult.removeChild(paraResult);
+
+};
+
+const clearScore = () => {
+
+  playerScore = 0;
+
+  computerScore = 0;
+
+};
